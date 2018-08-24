095e355b597cf65783987c1228748a5a0c768526
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-08-24 03:19:41 +0000

    Create jquery post logic template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,4 @@
 $(function() {
-
   function createForm() {
     var output = $('<form>')
       .attr('class', 'dropdown')
@@ -25,6 +24,7 @@ $(function() {
       .attr('aria-expanded', 'false');
 
     var editIcon = createEditIcon()
+
     output.append(editIcon);
     return output;
   }
@@ -62,21 +62,57 @@ $(function() {
     return output;
   }
 
-  function createListElement(todo) {
+  function createListElement(todoText) {
     var output = $('<li>')
-    .attr('class', 'list-group-item')
-    .text(todo);
+      .attr('class', 'list-group-item')
+      .text(todoText);
 
-    var form = createForm()
-    output.append(form)
+    var form = createForm();
+    output.append(form);
     return output;
   }
 
-  $.get('todos').done(function() {
+  function renderList(listName, todoText) {
+    var listElement = createListElement(todoText);
 
+    switch (listName) {
+      case "To Eat":
+        $('#to-eat').append(listElement);
+        break;
+      case "To Watch":
+        $('#to-watch').append(listElement);
+        break;
+      case "To Read":
+        $('#to-read').append(listElement);
+        break;
+      case: "To Buy":
+        $('#to-buy').append(listElement);
+        break;
+      default:
+        $('#uncategorized').append(listElement);
+        break;
+    }
+  }
+
+  $("form#new-list-item").on("submit", function(event) {
+    event.preventDefault();
+
+    var todoElement = $(this).children("input");
+    var todoText = todoElement.val();
+
+    /*
+      API LOGIC HERE
+    */
+
+    var category;
+
+    $.post('/todos').done(function() {
+      todoText.val("");
+      renderList("To Eat", todoText);
+    });
   });
 
-
+  // Event delegation example. Needs to be replaced with delete/complete code
   $('ul.list-group').on('click', 'li', function(event) {
     var li = $(event.target);
     li.toggleClass('done');
